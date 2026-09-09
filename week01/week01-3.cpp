7293ce0ece5eab1be3415b2d60f850b7a062c7db
victor20061225-maker <victor20061225@gmail.com>
2026-09-09 11:56:34 +0800

    week01

---
+++ week01/week01-3.cpp
@@ -0,0 +1,15 @@
+//week01-3.cpp 學習計畫 Basic 第3題
+//LeetCode 28. Find the Index of the First Occurrence in a String
+//大海撈針 (在一大堆草堆裡,找一枝針)
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        //所有的程式題目,都可以用 for(迴圈) if(判斷)函式呼叫    
+        int N1 = haystack.length(),N2 = needle.length(); // 函式呼叫
+        for(int i=0; i<= N1-N2 ; i++) { // 迴圈
+            if ( haystack.substr(i,N2) == needle ) return i;//找到答案
+            //如果 大字串 的 .substr(開始,長度) 等於 小字串, 就找到答案了。
+        }
+        return -1; // 找不到    
+    }
+};
